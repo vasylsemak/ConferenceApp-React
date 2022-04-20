fb536d1c50030cd25b49c81c4633a733689829d0
vasylsemak <vasylsmk1401@gmial.com>
2022-04-20 13:48:51 -0700

    delete logs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "df21c0fc2f5803da"; }
+/******/ 		__webpack_require__.h = function() { return "a4db578760c3459b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
